cd54c4216f8d10c25d4f257ca921564ef43411b7
huxaiphaer <l654321123>
2019-07-28 10:59:58 +0300

    fix edit bug

--- ICFERApp/Repository/StudentRepository.cs
+++ ICFERApp/Repository/StudentRepository.cs
@@ -39,7 +39,6 @@ namespace ICFERApp.Repository
             {
                 // updating student.
 
-//             _context.Entry(existingStudent).Property(x => x.ImageUrl).IsModified = false; 
                _context.Entry(existingStudent).CurrentValues.SetValues(student);
                _context.Entry(existingStudent).State = EntityState.Modified;
                
@@ -109,6 +108,48 @@ namespace ICFERApp.Repository
                 student.ImageUrl = existingStudent.ImageUrl;
                 _context.Entry(existingStudent).CurrentValues.SetValues(student);
                 _context.Entry(existingStudent).State = EntityState.Modified;
+                
+                 // updating parents.
+               existingStudent.Parents.ReligionOfDeceasedFather = student.Parents.ReligionOfDeceasedFather;
+               existingStudent.Parents.ReligionOfDeceasedMother = student.Parents.ReligionOfDeceasedMother;
+               existingStudent.Parents.DateOfDemiseOfFather = student.Parents.DateOfDemiseOfFather;
+               existingStudent.Parents.DateOfDemiseOfMother = student.Parents.DateOfDemiseOfMother;
+               existingStudent.Parents.NamesOfMother = student.Parents.NamesOfMother;
+               existingStudent.Parents.NamesOfFather = student.Parents.NamesOfFather;
+               existingStudent.Parents.ReligionOfMother = student.Parents.ReligionOfMother;
+               existingStudent.Parents.MaritalStatusOfMother = student.Parents.MaritalStatusOfMother;
+               existingStudent.Parents.OccupationOfMother = student.Parents.OccupationOfMother;
+               existingStudent.Parents.MonthlyIncome = student.Parents.MonthlyIncome;
+               
+
+               // updating guardian.
+
+               existingStudent.Guardian.FirstName = student.Guardian.FirstName;
+               existingStudent.Guardian.MiddleName = student.Guardian.MiddleName;
+               existingStudent.Guardian.LastName = student.Guardian.LastName;
+               existingStudent.Guardian.Comments = student.Guardian.Comments;
+               existingStudent.Guardian.Occupation = student.Guardian.Occupation;
+               existingStudent.Guardian.RelationshipToOrphan = student.Guardian.RelationshipToOrphan;
+               existingStudent.Guardian.MonthlyIncome = student.Guardian.MonthlyIncome;
+               existingStudent.Guardian.EmployersName = student.Guardian.EmployersName;
+               existingStudent.Guardian.WorkAddress = student.Guardian.WorkAddress;
+               existingStudent.Guardian.MobileNo = student.Guardian.MobileNo;
+               existingStudent.Guardian.PhysicalLocation = student.Guardian.PhysicalLocation;
+               
+
+                //updating education.
+                
+                existingStudent.Education.School = student.Education.School;
+                existingStudent.Education.Telephone = student.Education.Telephone;
+                existingStudent.Education.HeadTeacher = student.Education.HeadTeacher;
+                existingStudent.Education.EducationLevel = student.Education.EducationLevel;
+                existingStudent.Education.AddressOfSchool = student.Education.AddressOfSchool;
+                
+                //updating sibling.
+                
+                existingStudent.Siblings.NumberOfBrothers = student.Siblings.NumberOfBrothers;
+                existingStudent.Siblings.NumberOfSisters = student.Siblings.NumberOfSisters;
+                
                 _context.SaveChanges();
             }
            
